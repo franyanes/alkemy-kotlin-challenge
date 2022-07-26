ce10f02d50ab5852f0df00a17547f09462f27a3b
Francisco Yanes <franciscoyanesk@gmail.com>
2022-07-26 15:10:39 -0300

    Add View Model for **Movie Details**

---
+++ app/src/main/java/com/example/moviecatalog/ui/single_movie/MovieDetailsViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.moviecatalog.ui.single_movie
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.moviecatalog.data.repository.NetworkState
+import com.example.moviecatalog.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDetailsViewModel(private val movieRepository: MovieDetailsRepository, movieId: Int): ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+    /* We use `lazy` so get the movie details when we need them and not when we instantiate the class.
+    * It would work without `lazy` regardless, but we use it for better performance. */
+    val movieDetails: LiveData<MovieDetails> by lazy {
+        movieRepository.fetchSingleMovieDetails(compositeDisposable, movieId)
+    }
+    val networkState: LiveData<NetworkState> by lazy {
+        movieRepository.getMovieDetailsNetworkState()
+    }
+
+    /* The function `onCleared()` gets called when the activity or fragment gets destroyed,
+    * so on this function we will dispose of our composite disposable to avoid memory leaks. */
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
