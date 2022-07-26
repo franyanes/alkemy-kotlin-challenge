c926a32ac27ce2e516a07fdb36f0195b2b2a97d5
Francisco Yanes <franciscoyanesk@gmail.com>
2022-07-25 23:32:33 -0300

    Add **Movie Details data class** generated from JSON response

---
+++ app/src/main/java/com/example/moviecatalog/data/vo/Genre.kt
@@ -0,0 +1,11 @@
+package com.example.moviecatalog.data.vo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Genre(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
+)
