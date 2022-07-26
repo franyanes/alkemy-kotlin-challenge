6286d7d43377aab209b7052cc25a709fe8a5c6c7
Francisco Yanes <franciscoyanesk@gmail.com>
2022-07-26 15:56:04 -0300

    Add button to Main Activity to test the API when fetching Movie Details

--- app/src/main/java/com/example/moviecatalog/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalog/ui/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.moviecatalog.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.moviecatalog.R
+import com.example.moviecatalog.ui.single_movie.MovieDetailsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Placeholder to test the API
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            val intent = Intent(this, MovieDetailsActivity::class.java)
+            intent.putExtra("id", 299534)
+            this.startActivity(intent)
+        }
     }
 }
