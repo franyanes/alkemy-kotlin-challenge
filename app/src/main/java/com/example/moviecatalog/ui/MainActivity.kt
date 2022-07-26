541a8fbb1491b6be2c7db0abb4795c8c2c8330fe
Francisco Yanes <franciscoyanesk@gmail.com>
2022-07-25 22:21:38 -0300

    Add Movie Details Activity and layout

--- app/src/main/java/com/example/moviecatalog/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalog/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moviecatalog
+package com.example.moviecatalog.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moviecatalog.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
