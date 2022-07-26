fa3f8718d6a4bf1589cdb2a5c04579bc7d73ff15
Francisco Yanes <franciscoyanesk@gmail.com>
2022-07-26 18:27:07 -0300

    Add method to get Popular Movies from the API

--- app/src/main/java/com/example/moviecatalog/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/example/moviecatalog/data/api/TheMovieDBInterface.kt
@@ -1,12 +1,19 @@
 package com.example.moviecatalog.data.api
 
 import com.example.moviecatalog.data.vo.movie_details.MovieDetails
+import com.example.moviecatalog.data.vo.popular_movies.PopularMovies
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 /* This interface will be used to fetch the data from the API. */
+
 interface TheMovieDBInterface {
+    /* `Query` will link the `page` parameter to the page item in the URL. */
+    @GET("movie/popular")
+    fun getPopularMovies(@Query("page")page: Int): Single<PopularMovies>
+
     /* `Path` links the `movie_id` from the GET to the `id` parameter. */
     /* Single is one type of observable in Reactivex or RxJava.
     *  It emits a single value (instead of multiple), or an error. */
