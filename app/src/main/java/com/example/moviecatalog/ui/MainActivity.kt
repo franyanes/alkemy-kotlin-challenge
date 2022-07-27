91b84e305d0765b599cac34c3d590a4e764ee9cd
Francisco Yanes <franciscoyanesk@gmail.com>
2022-07-26 23:48:16 -0300

    Add Popular Movies List XML layouts

--- app/src/main/java/com/example/moviecatalog/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalog/ui/MainActivity.kt
@@ -11,12 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Placeholder to test the API
-        findViewById<Button>(R.id.btn).setOnClickListener {
-            val intent = Intent(this, MovieDetailsActivity::class.java)
-            intent.putExtra("id", 299534)
-            this.startActivity(intent)
-        }
+//        // Placeholder to test the API
+//        findViewById<Button>(R.id.btn).setOnClickListener {
+//            val intent = Intent(this, MovieDetailsActivity::class.java)
+//            intent.putExtra("id", 299534)
+//            this.startActivity(intent)
+//        }
     }
 }
