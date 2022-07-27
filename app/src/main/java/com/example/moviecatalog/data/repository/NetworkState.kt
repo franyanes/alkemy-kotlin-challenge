33fa0c7ffe316d5bc1126b44b459a7f4ca47205b
Francisco Yanes <franciscoyanesk@gmail.com>
2022-07-27 00:10:20 -0300

    Add new Network State for End of List error

--- app/src/main/java/com/example/moviecatalog/data/repository/NetworkState.kt
+++ app/src/main/java/com/example/moviecatalog/data/repository/NetworkState.kt
@@ -13,5 +13,6 @@ class NetworkState(val status: Status, val msg: String) {
         val LOADED: NetworkState = NetworkState(Status.SUCCESS, "Success")
         val LOADING: NetworkState = NetworkState(Status.RUNNING, "Running")
         val ERROR: NetworkState = NetworkState(Status.FAILED, "Something went wrong")
+        val EOL: NetworkState = NetworkState(Status.FAILED, "End of list")
     }
 }
