41263829fc59f782f0f497c3b9c433acb2b17672
Francisco Yanes <franciscoyanesk@gmail.com>
2022-07-28 19:42:00 -0300

    Style: Fix comment

--- app/src/main/java/com/example/moviecatalog/data/repository/NetworkState.kt
+++ app/src/main/java/com/example/moviecatalog/data/repository/NetworkState.kt
@@ -9,7 +9,7 @@ enum class Status {
 class NetworkState(val status: Status, val msg: String) {
     companion object {
         /* We use companion objects when we want something to be static.
-         * Now we can these three variables without instancing the class. */
+         * Now we can use these variables without instancing the class. */
         val LOADED: NetworkState = NetworkState(Status.SUCCESS, "Success")
         val LOADING: NetworkState = NetworkState(Status.RUNNING, "Running")
         val ERROR: NetworkState = NetworkState(Status.FAILED, "Something went wrong")
